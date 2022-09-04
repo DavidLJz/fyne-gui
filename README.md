5275d26498ff453ed506927efeb043d69aa73adf
DavidLJz <4dlj1995@gmail.com>
2022-09-04 15:10:28 -0500

    cambios

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fyne-gui
 Testing Fyne Library for Go
+
+https://www.youtube.com/watch?v=l9TGGU79Y04
--- main.go
+++ main.go
@@ -1,14 +1,72 @@
 package main
 
 import (
+	"encoding/json"
+	"image/color"
+	"net/http"
+	"time"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+var client *http.Client
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+type randomFact struct {
+	Text string `json:"text"`
+}
+
+func getRandomFact() (randomFact, error) {
+	var fact randomFact
+	resp, err := client.Get("https://uselessfacts.jsph.pl/random.json?language=en")
+	if err != nil {
+		return randomFact{}, nil
+	}
+
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&fact)
+	if err != nil {
+		return randomFact{}, err
+	}
+
+	return fact, nil
+}
+
+func main() {
+	client = &http.Client{Timeout: 10 * time.Second}
+
+	a := app.New()
+	w := a.NewWindow("New Useless Fact")
+	w.Resize(fyne.NewSize(800, 300))
+
+	title := canvas.NewText("Get your useless fact", color.Black)
+	title.TextStyle = fyne.TextStyle{
+		Bold: true,
+	}
+	title.TextSize = 24
+	title.Alignment = fyne.TextAlignCenter
+
+	factTxt := widget.NewLabel("")
+	factTxt.Wrapping = fyne.TextWrapWord
+
+	button := widget.NewButton("Get Fact", func() {
+		fact, err := getRandomFact()
+		if err != nil {
+			dialog.ShowError(err, w)
+		} else {
+			factTxt.SetText(fact.Text)
+		}
+	})
+
+	hBox := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), button, layout.NewSpacer())
+	vBox := container.New(layout.NewVBoxLayout(), title, hBox, widget.NewSeparator(), factTxt)
+
+	w.SetContent(vBox)
 	w.ShowAndRun()
 }
